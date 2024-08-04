af07c58b99a328861218aea98c5e754c8b9858c3
MikeG112 <mgartner112@gmail.com>
2024-08-04 01:33:53 -0400

    Add updated dataset generator

--- generate_outcome_dataset.py
+++ generate_outcome_dataset.py
@@ -3,15 +3,14 @@ import argilla as rg
 from argilla.client.feedback.schemas import SuggestionSchema, SpanValueSchema
 
 import json
+import os
 
-ARGILLA_API_URL = "http://localhost:6900"
-ARGILLA_API_KEY = "argilla.apikey"
-
+ARGILLA_API_URL = os.environ.get("ARGILLA_API_URL", "http://localhost:6900")
+ARGILLA_API_KEY = os.environ.get("DEFAULT_USER_API_KEY", "argilla.apikey")
 
 
 def construct_feedback_dataset() -> rg.FeedbackDataset:
-
-    guidelines = """Select the spans for all specified subsections of the text, if they exist. TODO: specify punctuation rules, etc.\n
+    guidelines = """Select the spans associated with the background text for the case.
 
     Include terminal punctuation for full sentence spans.\n
 
@@ -19,7 +18,7 @@ def construct_feedback_dataset() -> rg.FeedbackDataset:
 
 
     **Background Context**
-    This is the maximal case detail which is known at the time of the appeal review. This includes, but is not necessarily limited
+    This is (ideally the maximal) case detail which is known at the time of the appeal review. This includes, but is not necessarily limited
     to, all details provided that describe the situation leading up to the adverse coverage denial, and all actions taken by the insurer
     before an appeal was submitted to an independent reviewer.
 
@@ -28,114 +27,130 @@ def construct_feedback_dataset() -> rg.FeedbackDataset:
     within background context.
     """
 
-    # Span Based extractive QA dataset
     ds = rg.FeedbackDataset(
-        guidelines="Select the spans for all specified subsections of the text, if they exist.",
+        guidelines=guidelines,
         fields=[
             rg.TextField(name="case-summary", title="Case Summary"),
-            rg.TextField(name="market-segment", title="Market Segment"),
             rg.TextField(name="final-outcome", title="Final Outcome"),
         ],
         questions=[
             rg.SpanQuestion(
                 name="case-spans",
-                title="Highlight the entities in the text:",
+                title="Highlight spans associated with the following quantities in the text:",
                 labels={
                     "BC": "Background Context",
                     "DG": "Diagnosis",
                     "SVC": "Service",
-                    "RTL": "Adjudication Rationale",
-                    "OUTCOME": "Adjudication Recommendation",
-                    "DNLRTL": "Denial Rationale",
+                    "RV-DC": "Reviewer's Decision",
                 },
                 field="case-summary",
                 required=True,
-                visible_labels=6,
-                allow_overlapping=True
-            )
+                visible_labels=4,
+                allow_overlapping=True,
+            ),
+            rg.RatingQuestion(
+                name="info-sufficiency",
+                title="Is the Background Context you've highlighted sufficient to determine the case outcome?",
+                description="Use a scale from 1 to 5, 1 being the information is certainly insufficient, 5 being certainly sufficient.",
+                values=[1, 2, 3, 4, 5],
+                required=False,
+            ),
         ],
     )
     return ds
 
 
-if __name__ == "__main__":
-    # Construct a dataset
-    target_workspace = "case-outcomes2"
-
+def make_workspace(ws_name: str) -> None:
     # Create workspace if it doesn't exist
     try:
-        workspace = rg.Workspace.create(target_workspace)
+        _workspace = rg.Workspace.create(ws_name)
     except ValueError:
-        pass
+        print(f"Skipping creation of workspace {ws_name} as it already exists.")
 
-    rg.init(
-        api_url=ARGILLA_API_URL, api_key=ARGILLA_API_KEY, workspace=target_workspace
-    )
+    return None
 
-    # Create new users
-    # user = rg.User.create(
-    #     username="new-user",
-    #     first_name="New",
-    #     last_name="User",
-    #     password="new-password",
-    #     role="admin",
-    #     workspaces=["ws1", "ws2"])
 
-    name = "case-outcomes"
-    ds = construct_feedback_dataset()
-
+def construct_ds_records(jsonl_path: str) -> list:
+    """From a standardized jsonl format (namely that of HICRIC processed files),
+    construct a list of records to submit into an argilla dataset of the format above.
+    """
     # Add records
     records = []
-    jsonl_path = "./data/outcomes/nydfs.jsonl"
     with open(jsonl_path, "r") as jsonl_file:
         for idx, line in enumerate(jsonl_file):
             json_obj = json.loads(line)
             full_text = json_obj["text"]
-            appeal_type = json_obj["appeal_type"]
-            outcome = json_obj["decision"]
-            background = json_obj["background"]
-            # justification = json_obj["justification"]
+            outcome = json_obj.get("decision", "unknown")
 
-            # Calculate start / end indices
-            background_start = full_text.index(background)
-            background_end = background_start + len(background)
-            if background_start >= background_end:
-                continue
+            # TODO: come up with some good initial rules
+            if include_suggestions:
+                background = ""
+                justification = ""
+                # Calculate start / end indices
+                background_start = full_text.index(background)
+                background_end = background_start + len(background)
+                if background_start >= background_end:
+                    continue
 
-            # justification_start = full_text.index(justification)
-            # justification_end = justification_start + len(justification)
-            # if justification_start >= justification_end:
-            #     continue
+                justification_start = full_text.index(justification)
+                justification_end = justification_start + len(justification)
+                if justification_start >= justification_end:
+                    continue
 
             record = rg.FeedbackRecord(
                 fields={
                     "case-summary": full_text,
-                    "market-segment": appeal_type,
                     "final-outcome": outcome,
                 },
-                suggestions=[
-                    SuggestionSchema(
-                        question_name="case-spans",
-                        value=[
-                            SpanValueSchema(
-                                start=background_start,  # position of the first character of the span
-                                end=background_end,  # position of the character right after the end of the span
-                                label="BC",
-                                score=0.9,
-                            ),
-                            # SpanValueSchema(
-                            #     start=justification_start,  # position of the first character of the span
-                            #     end=justification_end,  # position of the character right after the end of the span
-                            #     label="RTL",
-                            #     score=0.9,
-                            # ),
-                        ],
-                        agent="nydfs-manual-rules-v0.0.0",
-                    ),
-                ],
             )
+            if include_suggestions:
+                suggestions = (
+                    [
+                        SuggestionSchema(
+                            question_name="case-spans",
+                            value=[
+                                SpanValueSchema(
+                                    start=background_start,  # position of the first character of the span
+                                    end=background_end,  # position of the character right after the end of the span
+                                    label="BC",
+                                    score=0.9,
+                                )
+                            ],
+                            agent="annotator-bot-0.0.1",
+                        ),
+                    ],
+                )
+                record.suggestions = suggestions
+
             records.append(record)
 
-    ds.add_records(records)
+    return records
 
-    ds.push_to_argilla(name=name, workspace=target_workspace)
+
+if __name__ == "__main__":
+    # Config
+    include_suggestions = False  # Include suggestions
+
+    rg.init(api_url=ARGILLA_API_URL, api_key=ARGILLA_API_KEY)
+
+    # Make a workspace
+    target_workspace = "case-summaries"
+    make_workspace(target_workspace)
+
+    rg.set_workspace(target_workspace)
+
+    # Construct a dataset for each raw source:
+    sources = [
+        ("NY DFS External Appeal Database", "./data/processed/nydfs.jsonl"),
+        ("CA CDI External Appeal Database", "./data/processed/ca_cdi/summaries/aggregate.jsonl"),
+        (
+            "CA DMHC External Appeal Database",
+            "./data/processed/independent-medical-review-imr-determinations-trend.jsonl",
+        ),
+    ]
+
+    for ds_name, path in sources:
+        ds = construct_feedback_dataset()
+        records = construct_ds_records(path)
+        ds.add_records(records)
+        ds.push_to_argilla(name=ds_name, workspace=target_workspace)
