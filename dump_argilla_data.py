b5d653d0bdd51064d2ae4d7f8877ff55aaa374b2
MikeG112 <mgartner112@gmail.com>
2024-08-03 23:17:19 -0400

    Clean dump script

--- dump_argilla_data.py
+++ dump_argilla_data.py
@@ -1,10 +1,51 @@
+import argparse
+import os
+
 import argilla as rg
 
-# Load annotated dataset from the argilla server
-ds_name = "case-outcomes"
-workspace = "case-outcomes"
-local_dataset = rg.FeedbackDataset.from_argilla(name=ds_name, workspace=workspace)
 
-# Export argilla dataset to a datasets Dataset
-dataset_ds = local_dataset.format_as("datasets")
-dataset_ds.to_json(f"./data/annotated/{ds_name}.jsonl")
+def main(dataset: str, workspace: str, outpath: str | None) -> None:
+    # Load an annotated dataset from the argilla server
+    local_dataset = rg.FeedbackDataset.from_argilla(name=dataset, workspace=workspace)
+
+    # Export argilla dataset to a (huggingface) datasets Dataset
+    dataset_ds = local_dataset.format_as("datasets")
+
+    # Dump that to json on disk
+    if outpath is None:
+        outpath = f"./data/annotated/{dataset}.jsonl"
+
+    os.makedirs(outpath, exist_ok=True)
+    dataset_ds.to_json(outpath)
+
+    return None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="case-summaries",
+        help="Name of argilla dataset to dump.",
+    )
+    parser.add_argument(
+        "--workspace",
+        type=str,
+        default="case-summaries",
+        help="Name of workspace for dataset",
+    )
+    parser.add_argument(
+        "--outpath",
+        type=str,
+        default=None,
+        help="Optional output path.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    args_dict = vars(args)
+    main(**args_dict)
