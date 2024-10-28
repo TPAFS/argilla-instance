38a8711e5ae2e49fa70c509c1047400bcc815fd3
MikeG112 <mgartner112@gmail.com>
2024-10-28 01:00:23 -0400

    Update guidelines

--- generate_outcome_dataset.py
+++ generate_outcome_dataset.py
@@ -12,50 +12,62 @@ ARGILLA_API_KEY = os.environ.get("ARGILLA_API_KEY", "argilla.apikey")
 def construct_feedback_dataset() -> rg.FeedbackDataset:
     guidelines = """
 
-    ## **Span Selection**
+## **Span Selection**
 
-    ### General Guidance
-    Select spans associated with each of four token-type classes.
+### General Guidance
 
-    The four token-types, and guideline descriptions for each are:
+Select spans associated with each of four token-type classes.
 
-    - **Background Context**
+The four token-types, and guideline descriptions for each are:
 
-    This is (ideally the maximal) case detail which is known at the time of the appeal review. This includes, but is not necessarily limited to, all details provided that describe the situation leading up to the adverse coverage denial, and all actions taken by the insurer _before_ an appeal was submitted to an independent reviewer. This should not include information which leaks the adjudication outcome, which constitutes the opinion of the reviewer, or which could not be known prior to appeal submission.
+- **Background Context**  
+    
+  This is (ideally the maximal) case detail which is known at the time of the appeal *submission*. This includes, but is not necessarily limited to, all details provided that describe the situation leading up to the adverse coverage denial, and all actions taken by the insurer *before* an appeal was submitted to an independent reviewer. This should not include information which leaks the adjudication outcome, which constitutes the opinion of the reviewer, or which could not be known prior to appeal submission.  
+    
+- **Diagnosis**  
+    
+  This should be some indication of the diagnosis associated with an appeal. It could be a diagnosis code, a plain text but precise description, or a broad category of diagnoses (e.g. "cancer"). Some reviews do not detail the relevant diagnosis.  
+    
+- **Service**  
+    
+  This should be some indication of the service associated with an appeal. It could be a procedure code, a plain text but precise description, or a broad category of services (e.g. "oral surgery"). Some reviews do not detail the relevant services.  
+    
+- **Reviewer's Decision**  
+    
+  This should be some indication of the decision of the reviewer writing the summary. This could be a single sentence in the review such as "The insurer's denial should be Overturned", or a more implicit decision, such as "The insurer did not act in the best interest of the patient". Note that reviewer's decisions need not agree with the final outcome, as sometimes multiple reviewers vote on the outcome in a majority rule fashion.
 
-    - **Diagnosis**
+*Note*: Spans (or subspans) can be given multiple labels, or zero labels. The labels are not mutually exclusive, nor are they exhaustive. For example, a diagnosis is likely to occur *within* background context.
 
-    This should be some indication of the diagnosis associated with an appeal. It could be a diagnosis code, a plain text but precise description, or a broad category of diagnoses (e.g. "cancer"). Some reviews do not detail the relevant diagnosis.
+#### Additional Details
 
-    - **Service**
+- Include terminal punctuation for full sentence spans.  
+- The background context span labeling task is particularly subtle, as there are often summary descriptions of background context made by a reviewer imbued with potentially leaking, opinion based, or subjective language. When in doubt about whether a background span leaks the adjudication rationale or decision of a reviewer, do **not** highlight the background context.
 
-    This should be some indication of the service associated with an appeal. It could be a procedure code, a plain text but precise description, or a broad category of services (e.g. "oral surgery"). Some reviews do not detail the relevant services.
+## **Background Sufficiency**
 
-    - **Reviewer's Decision**
+### General Guidance
 
-    This should be some indication of the decision of the reviewer writing the summary. This could be a single sentence in the review such as "The insurer's denial should be Overturned", or a  more implicit decision, such as "The insurer did not act in the best interest of the patient". Note that reviewer's decisions need not agree with the final outcome, as sometimes multiple reviewers vote on the outcome in a majority rule fashion.
+Rate the selected background context span on a scale from 1 to 5, to indicate the degree to which the background context alone is sufficient to make an informed guess about the likelihood of denial overturn.
 
+#### Additional Details
 
-    _Note_: Spans (or subspans) can be given multiple labels, or zero labels. The labels are not mutually exclusive, nor are they exhaustive. For example, a diagnosis is likely to occur _within_ background context.
+This is a highly subjective matter. In general, none of the case descriptions included in these datasets are sufficient to make case adjudication determinations or accurately predict perfectly what the decisions are likely to be. This is because they uniformly lack context such as diagnosis and procedural codes, office and chart notes, detailed patient medical history, information about the exact legal jurisdiction relevant to the case, a full copy of the insured's insurance contract, etc, etc.
 
-    #### Additional Details
+The goal here is to nonetheless assign *relative* sufficiency scores from 1-5, that adhere to a consistent relative annotation standard for any given annotator. Any differences that might arise in the relative scales used by different annotators will be evaluated and controlled for after the fact.
 
-    -  Include terminal punctuation for full sentence spans.
-    - The background context span labeling task is particularly subtle, as there are often summary descriptions of background context made by a reviewer imbued with potentially leaking, opinion based, or subjective language. When in doubt about whether a background span leaks the adjudication rationale or decision of a reviewer, do **not** highlight the background context.
+As normalizing guideposts, let's begin by discussing the extreme cases. It is clear, for example, that some case description backgrounds are completely insufficient to make an informed guess about overturn likelihood. For example, some case backgrounds say only completely general things like "A patient was denied inpatient admission". There are a multitude of reasons why one could be denied inpatient admission that would result in overturn and a multitude of reasons that would not. Cases with only such completely unspecified backgrounds should receive a score of 1\. On the other hand, the most detailed case backgrounds will include detailed diagnosis and service descriptions, as well as lengthy, detailed explanation about what led to the care event, what the patient's medical records suggest about the medical details of the event, why the insurer has issued a denial, and why the patient or physician believes overturn is merited. Such cases should generally receive a score of 4 or 5\.
 
-    ## **Background Sufficiency**
+Let's now turn to a specific rubric we've developed to help make the sufficiency determination. We reiterate what was stated earlier: a score of 5 does *not* mean that a case background description is sufficient to determine whether a case will be overturned. It simply means that it belongs to the class of background descriptions which is closest to sufficient, relative to other background descriptions.
 
-    ### General Guidance
+| Score | Criteria | Example |
+| :---- | :---- | :---- |
+| 1 | Little to no specific context for the denial is provided. The description can easily be extended to describe cases meriting coverage, and extended to describe cases not meriting coverage. Neither a specific condition, specific service or treatment, nor specific drug is stated in the description. | "A patient was denied coverage for an inpatient admission." |
+| 2 | Some specific context for the denial is provided, but the information can still be easily extended to describe cases meriting coverage, and extended to cases not meriting coverage. At a minimum, the description mentions either the specific condition / medical situation being treated, *or* the specific service / treatment. | "An enrollee has requested bilateral sacroiliac (SI) joint fusion surgery for treatment of her medical condition." |
+| 3 | Some specific context for the denial is provided. The information may or may not be able to be easily extended to describe cases meriting coverage and extended to cases not meriting coverage. Understanding whether or not such dual extension is possible may require medical expertise. At a minimum, the description mentions the specific condition / medical situation being treated, *and* the specific service / treatment. | "The parent of a male enrollee has requested authorization and coverage for immunotherapy with nivolumab (Opdivo). The Health Plan has denied this request indicating that the requested medication is considered investigational for treatment of the enrollees grade IV glioblastoma." |
+| 4 | Some context for the denial has been provided. At a minimum the context includes the condition / medical situation being treated, the service / treatment being used, and at least some description of the specific events or rationale leading to the coverage request. It would be moderately difficult to extend the description in any realistic way that would alter the sufficiency determination. | "An enrollee requested a power seat elevator for her power wheelchair for treatment of the enrollee, who has a history of a spinal cord injury. With power seat elevation, the patient would be able to adjust the seat of her wheelchair to be level with the transfer surface, to perform a level surface transfer independently with a sliding board." |
+| 5 | A (relatively) complete context for the denial has been provided. At a minimum the context includes the condition / medical situation being treated, the service / treatment being used, and at least some description of the specific events or rationale leading to the coverage request. It would be extremely difficult to extend the description in any realistic way that would alter the sufficiency determination. | “This patient has extensive and inoperable carcinoma of the stomach. He was started on chemotherapy with Xeloda and Oxaliplatin, because he has less nausea with Oxaliplatin than with the alternative, Cisplatin. Oxaliplatin was denied as experimental for treatment of his gastric cancer.” |
 
-    Rate the selected background context span on a scale from 1 to 5, to indicate the degree to which the background context alone is sufficient to make an informed guess about the likelihood of denial overturn.
-
-    #### Additional Details
-    This is a highly subjective matter. In general, none of the case descriptions included in these datasets are completely sufficient to make case adjudication determinations of accurately predict perfectly what the decisions are likely to be. This is because they uniformly lack context such as diagnosis and procedural codes, office and chart notes, patient medical history, etc, etc.
-
-    The goal here is primarily to retain _relative_ sufficiency estimates, on a per-annotator basis.
-
-    As normalizing guideposts, let's discuss the extreme cases. It is clear, for example, that some case description backgrounds are completely insufficient to make an informed guess about overturn likelihood. For example, some case backgrounds say only completely general things like "A patient was denied inpatient admission". There are a multitude of reasons why one could be denied inpatient admission that would result in overturn and a multitude of reasons that would not. Cases with only such completely unspecific backgrounds should receive a score of 1. On the other hand, the most detailed case backgrounds will include detailed diagnosis and service descriptions, as well as lengthy, detailed explanation about what led to the care event, what the patient's medical records suggest about the medical details of the event, why the insurer has issued a denial, and wh"""
-
+"""
     ds = rg.FeedbackDataset(
         guidelines=guidelines,
         fields=[
@@ -105,18 +117,25 @@ def construct_ds_records(jsonl_path: str, include_suggestions: bool) -> list:
     """
 
     add_existing_annotations = True
-    add_to_userid = str(rg.User.me().id)  # Add existing annotations to user running script
+    add_to_userid = str(
+        rg.User.me().id
+    )  # Add existing annotations to user running script
 
     # Used to add previously annotated records
     if add_existing_annotations:
         annotations = {}
-        with open("./data/annotated/case-summaries-w-sufficiency.jsonl", "r") as jsonl_file:
+        with open(
+            "./data/annotated/case-summaries-w-sufficiency.jsonl", "r"
+        ) as jsonl_file:
             for idx, line in enumerate(jsonl_file):
                 json_obj = json.loads(line)
                 context = json_obj["context"].strip()
                 answers = json_obj.get("answers")
                 sufficiency_score = json_obj.get("sufficiency_score")
-                annotations[context] = {"answers": answers, "sufficiency_score": sufficiency_score}
+                annotations[context] = {
+                    "answers": answers,
+                    "sufficiency_score": sufficiency_score,
+                }
 
     # Add records
     records = []
@@ -166,7 +185,10 @@ def construct_ds_records(jsonl_path: str, include_suggestions: bool) -> list:
                         )
                         for (start, end) in zip(starts, ends)
                     ]
-                    values = {"case-spans": {"value": span_values}, "info-sufficiency": {"value": sufficiency_score}}
+                    values = {
+                        "case-spans": {"value": span_values},
+                        "info-sufficiency": {"value": sufficiency_score},
+                    }
 
                     record.responses = [
                         {
@@ -215,7 +237,10 @@ if __name__ == "__main__":
     # Construct a dataset for each raw source:
     sources = [
         ("NY DFS External Appeal Database", "./data/processed/nydfs.jsonl"),
-        ("CA CDI External Appeal Database", "./data/processed/ca_cdi/summaries/aggregate.jsonl"),
+        (
+            "CA CDI External Appeal Database",
+            "./data/processed/ca_cdi/summaries/aggregate.jsonl",
+        ),
         (
             "CA DMHC External Appeal Database",
             "./data/processed/independent-medical-review-imr-determinations-trend.jsonl",
